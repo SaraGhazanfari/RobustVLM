d53c396809394e45c40af58bb4312cacddfe5aa5
saraghazanfari <s.ghazanfari.ce@gmail.com>
2024-06-03 15:20:02 -0400

    layer bias added

--- train/adversarial_training_clip.py
+++ train/adversarial_training_clip.py
@@ -192,6 +192,7 @@ def main(args):
     # Freezing model start
     for name, param in model.named_parameters():
         if not name.startswith('visual.transformer.peft_bias'):
+            print(name)
             param.requires_grad = False
         else:
             print(name)
